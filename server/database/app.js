115c9e1927f229a5c59e1be9f8a7989d99b0fef4
Aniel Lorenci <anielorenci1@hotmail.com>
2025-03-26 12:54:10 -0400

    Dynamic Pages

--- server/database/app.js
+++ server/database/app.js
@@ -88,7 +88,7 @@ app.get('/fetchDealer/:id', async (req, res) => {
         }
         res.json(documents)
     } catch (error) {
-        res.status(500).json({ error: 'Error fetching dealler by ID' });
+        res.status(500).json({ error: 'Error fetching dealer by ID' });
     }
 });
 
