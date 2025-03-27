44a14d343a83b4d11a2fda8b3776b78d39953b13
Aniel Lorenci <anielorenci1@hotmail.com>
2025-03-26 21:20:56 -0400

    models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,7 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
+#from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -29,6 +29,7 @@ class CarMake(models.Model):
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
 
+
 class CarModel(models.Model):
     car_make = models.ForeignKey(
         CarMake, on_delete=models.CASCADE
@@ -44,7 +45,8 @@ class CarModel(models.Model):
         ('VAN', 'Van'),
         ]
     type = models.CharField(max_length=20, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023,
+    year = models.IntegerField(
+        default=2023,
         validators=[
             MaxValueValidator(2023),
             MinValueValidator(2015)
